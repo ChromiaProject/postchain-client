07976d71776d054e3267884d1e4bb26d63e119cf
Mikael Ståldal <mikael.staldal@chromaway.com>
2025-02-25 16:47:49 +0100

    Overridable defaults for configuration

--- postchain-client/src/main/kotlin/net/postchain/client/config/PostchainClientConfig.kt
+++ postchain-client/src/main/kotlin/net/postchain/client/config/PostchainClientConfig.kt
@@ -6,12 +6,12 @@ import net.postchain.client.request.RequestStrategyFactory
 import net.postchain.common.BlockchainRid
 import net.postchain.common.PropertiesFileLoader
 import net.postchain.common.config.Config
-import net.postchain.common.config.cryptoSystem
 import net.postchain.common.config.getEnvOrBooleanProperty
 import net.postchain.common.config.getEnvOrIntProperty
 import net.postchain.common.config.getEnvOrListProperty
 import net.postchain.common.config.getEnvOrLongProperty
 import net.postchain.common.config.getEnvOrStringProperty
+import net.postchain.common.reflection.newInstanceOf
 import net.postchain.crypto.CryptoSystem
 import net.postchain.crypto.KeyPair
 import net.postchain.crypto.Secp256K1CryptoSystem
@@ -25,6 +25,7 @@ const val MAX_RESPONSE_SIZE = 64 * 1024 * 1024 // 64 MiB
 val CONNECT_TIMEOUT: Duration = Duration.ofSeconds(60)
 val RESPONSE_TIMEOUT: Duration = Duration.ofSeconds(60)
 const val MAX_TX_SIZE = -1 // no limit
+const val MERKLE_HASH_VERSION = 1
 
 data class PostchainClientConfig @JvmOverloads constructor(
         val blockchainRid: BlockchainRid,
@@ -44,14 +45,18 @@ data class PostchainClientConfig @JvmOverloads constructor(
         val maxTxSize: Int = MAX_TX_SIZE,
         /** Will only be applied to synchronous client if enabled */
         val compressRequestBodies: Boolean = false,
-        val merkleHashVersion: Int = 1,
+        val merkleHashVersion: Int = MERKLE_HASH_VERSION,
 ) : Config {
     companion object {
-        @JvmStatic
-        fun fromProperties(propertiesFileName: String?): PostchainClientConfig =
-                fromConfiguration(propertiesFileName?.let { PropertiesFileLoader.load(it) } ?: BaseConfiguration())
+        val defaultConfig by lazy { PostchainClientConfig(BlockchainRid.ZERO_RID, EndpointPool.singleUrl("")) }
 
-        fun fromConfiguration(config: Configuration): PostchainClientConfig {
+        @JvmStatic
+        @JvmOverloads
+        fun fromProperties(propertiesFileName: String?, defaults: PostchainClientConfig = defaultConfig): PostchainClientConfig =
+                fromConfiguration(propertiesFileName?.let { PropertiesFileLoader.load(it) } ?: BaseConfiguration(), defaults)
+
+        @JvmOverloads
+        fun fromConfiguration(config: Configuration, defaults: PostchainClientConfig = defaultConfig): PostchainClientConfig {
             val pubkeys = config.getEnvOrListProperty("POSTCHAIN_CLIENT_PUBKEY", "pubkey", listOf())
             val privkeys = config.getEnvOrListProperty("POSTCHAIN_CLIENT_PRIVKEY", "privkey", listOf())
             require(pubkeys.size == privkeys.size) { "Equally many pubkeys as privkeys must be provided, but ${pubkeys.size} and ${privkeys.size} was found" }
@@ -63,18 +68,27 @@ data class PostchainClientConfig @JvmOverloads constructor(
                             requireNotNull(config.getEnvOrStringProperty("POSTCHAIN_CLIENT_BLOCKCHAIN_RID", "brid")) { "Missing 'brid' (blockchain-rid)" }),
                     endpointPool = EndpointPool.default(apiUrls),
                     signers = signers,
-                    statusPollCount = config.getEnvOrIntProperty("POSTCHAIN_CLIENT_STATUS_POLL_COUNT", "status.poll-count", STATUS_POLL_COUNT),
-                    statusPollInterval = config.getEnvOrLongProperty("POSTCHAIN_CLIENT_STATUS_POLL_INTERVAL", "status.poll-interval", STATUS_POLL_INTERVAL.toMillis()).let { Duration.ofMillis(it) },
-                    failOverConfig = FailOverConfig.fromConfiguration(config),
-                    cryptoSystem = config.cryptoSystem(),
-                    maxResponseSize = config.getEnvOrIntProperty("POSTCHAIN_CLIENT_MAX_RESPONSE_SIZE", "max.response.size", MAX_RESPONSE_SIZE),
-                    connectTimeout = config.getEnvOrLongProperty("POSTCHAIN_CLIENT_CONNECT_TIMEOUT", "connect.timeout", CONNECT_TIMEOUT.toMillis()).let { Duration.ofMillis(it) },
-                    responseTimeout = config.getEnvOrLongProperty("POSTCHAIN_CLIENT_RESPONSE_TIMEOUT", "response.timeout", RESPONSE_TIMEOUT.toMillis()).let { Duration.ofMillis(it) },
-                    requestStrategy = config.getEnvOrStringProperty("POSTCHAIN_CLIENT_REQUEST_STRATEGY", "request.strategy", RequestStrategies.ABORT_ON_ERROR.name)
-                            .let { RequestStrategies.valueOf(it).factory },
-                    maxTxSize = config.getEnvOrIntProperty("POSTCHAIN_CLIENT_MAX_TX_SIZE", "max.tx.size", MAX_TX_SIZE), 
-                    compressRequestBodies = config.getEnvOrBooleanProperty("POSTCHAIN_CLIENT_COMPRESS_REQUEST_BODIES", "compress.requests", false),
-                    merkleHashVersion = config.getEnvOrIntProperty("POSTCHAIN_CLIENT_MERKLE_HASH_VERSION", "merkle-hash-version", 1),
+
+                    statusPollCount = config.getEnvOrIntProperty("POSTCHAIN_CLIENT_STATUS_POLL_COUNT", "status.poll-count",
+                            defaults.statusPollCount),
+                    statusPollInterval = config.getEnvOrLongProperty("POSTCHAIN_CLIENT_STATUS_POLL_INTERVAL", "status.poll-interval",
+                            defaults.statusPollInterval.toMillis()).let { Duration.ofMillis(it) },
+                    failOverConfig = FailOverConfig.fromConfiguration(config, defaults.failOverConfig),
+                    cryptoSystem = config.getEnvOrStringProperty("POSTCHAIN_CRYPTO_SYSTEM", "cryptosystem")?.let {
+                        newInstanceOf<CryptoSystem>(it)
+                    } ?: defaults.cryptoSystem,
+                    maxResponseSize = config.getEnvOrIntProperty("POSTCHAIN_CLIENT_MAX_RESPONSE_SIZE", "max.response.size",
+                            defaults.maxResponseSize),
+                    connectTimeout = config.getEnvOrLongProperty("POSTCHAIN_CLIENT_CONNECT_TIMEOUT", "connect.timeout",
+                            defaults.connectTimeout.toMillis()).let { Duration.ofMillis(it) },
+                    responseTimeout = config.getEnvOrLongProperty("POSTCHAIN_CLIENT_RESPONSE_TIMEOUT", "response.timeout",
+                            defaults.responseTimeout.toMillis()).let { Duration.ofMillis(it) },
+                    requestStrategy = config.getEnvOrStringProperty("POSTCHAIN_CLIENT_REQUEST_STRATEGY", "request.strategy")?.let {
+                        RequestStrategies.valueOf(it).factory
+                    } ?: defaults.requestStrategy,
+                    maxTxSize = config.getEnvOrIntProperty("POSTCHAIN_CLIENT_MAX_TX_SIZE", "max.tx.size", defaults.maxTxSize),
+                    compressRequestBodies = config.getEnvOrBooleanProperty("POSTCHAIN_CLIENT_COMPRESS_REQUEST_BODIES", "compress.requests", defaults.compressRequestBodies),
+                    merkleHashVersion = config.getEnvOrIntProperty("POSTCHAIN_CLIENT_MERKLE_HASH_VERSION", "merkle-hash-version", defaults.merkleHashVersion),
             )
         }
     }
