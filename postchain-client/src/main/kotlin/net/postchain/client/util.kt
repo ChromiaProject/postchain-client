8c8a0d818526c4130e7f29f6b59722c0438b553e
Eugene Tykulov <eugene.tykulov@chromaway.com>
2026-07-04 14:28:37 +0200

    Disable httpclient5 auto content decompression to fix gzip double-decompression

--- postchain-client/src/main/kotlin/net/postchain/client/util.kt
+++ postchain-client/src/main/kotlin/net/postchain/client/util.kt
@@ -16,17 +16,15 @@ import org.http4k.client.ApacheAsyncClient
 import org.http4k.client.ApacheClient
 import org.http4k.core.HttpHandler
 import org.http4k.core.then
-import org.http4k.filter.ClientFilters
 import org.http4k.filter.GzipCompressionMode
+import org.http4k.filter.RequestFilters
 
 fun bftMajority(n: Int) = n - (n - 1) / 3
 
 fun defaultHttpHandler(config: PostchainClientConfig): HttpHandler {
-    val compressionFilter = if (config.compressRequestBodies) {
-        ClientFilters.GZip(GzipCompressionMode.Streaming())
-    } else ClientFilters.AcceptGZip(GzipCompressionMode.Streaming())
-
-    return compressionFilter.then(ApacheClient(HttpClients.custom()
+    val httpClient = ApacheClient(HttpClients.custom()
+            .disableContentCompression() // response decompression is handled manually, since httpclient5 5.5+ no longer removes the content-encoding header when decompressing
+            .setDefaultHeaders(listOf(BasicHeader("accept-encoding", "gzip")))
             .setRetryStrategy(DefaultHttpRequestRetryStrategy(0, TimeValue.ZERO_MILLISECONDS)) // no retries
             .setConnectionManager(PoolingHttpClientConnectionManagerBuilder.create()
                     .setDefaultConnectionConfig(ConnectionConfig.custom()
@@ -38,11 +36,16 @@ fun defaultHttpHandler(config: PostchainClientConfig): HttpHandler {
                     .setCookieSpec(StandardCookieSpec.IGNORE)
                     .setResponseTimeout(Timeout.ofMilliseconds(config.responseTimeout.toMillis()))
                     .build())
-            .build()))
+            .build())
+
+    return if (config.compressRequestBodies) {
+        RequestFilters.GZip(GzipCompressionMode.Streaming()).then(httpClient)
+    } else httpClient
 }
 
 fun defaultAsyncHttpHandler(config: PostchainClientConfig) =
         ApacheAsyncClient(HttpAsyncClients.custom()
+                .disableContentCompression() // response decompression is handled manually, since httpclient5 5.5+ no longer removes the content-encoding header when decompressing
                 .setDefaultHeaders(listOf(BasicHeader("accept-encoding", "gzip")))
                 .setRetryStrategy(DefaultHttpRequestRetryStrategy(0, TimeValue.ZERO_MILLISECONDS)) // no retries
                 .setConnectionManager(PoolingAsyncClientConnectionManagerBuilder.create()
